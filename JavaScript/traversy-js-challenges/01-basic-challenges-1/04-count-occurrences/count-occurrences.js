e492b28113c69fcb01482473da0ee8492756e43e
Wilfred Erdo <wilfred05777@gmail.com>
2024-08-29 11:55:51 +0800

    first commit

---
+++ JavaScript/traversy-js-challenges/01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -0,0 +1,15 @@
+function countOccurrences(str, char) {
+   let count = 0;
+
+   for (let i = 0; i < structuredClone.length; i++) {
+      if (str[i] === char) {
+         count++;
+      }
+   }
+
+   return count;
+}
+
+const countOccurrences = (str, char) => str.split(char).length - 1;
+
+module.exports = countOccurrences;
