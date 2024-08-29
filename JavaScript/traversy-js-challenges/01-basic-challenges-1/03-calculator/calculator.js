ff5690fb944ce88e28c7272d51ad3fa65f217f90
Wilfred Erdo <wilfred05777@gmail.com>
2024-08-29 12:21:52 +0800

    8. Calculator testing

--- JavaScript/traversy-js-challenges/01-basic-challenges-1/03-calculator/calculator.js
+++ JavaScript/traversy-js-challenges/01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,27 @@
-function calculator() {}
+function calculator(num1, num2, operator) {
+    let result;
+
+
+    switch (operator) {
+        case "+":
+            result = num1 + num2;
+            break;
+        case "-":
+            result = num1 - num2;
+            break;
+        case "*":
+            result = num1 * num2;
+            break;
+        case "/":
+            result = num1 / num2;
+            break;
+        
+        default:
+            throw new error("Invalid operator")
+            break;
+    }
+
+    return result;
+}
 
 module.exports = calculator;
