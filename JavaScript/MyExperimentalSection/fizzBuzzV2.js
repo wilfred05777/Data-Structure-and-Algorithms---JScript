56aefda9d51f44d13ac901c425512f0ff29345f2
Wilfred Erdo Bancairen <wilfred05777@gmail.com>
2024-09-02 22:19:39 +0800

    prettier code adjust

--- JavaScript/MyExperimentalSection/fizzBuzzV2.js
+++ JavaScript/MyExperimentalSection/fizzBuzzV2.js
@@ -1,31 +1,21 @@
 function fizzBuzz(n) {
-    let result = []
+   let result = [];
 
-    for (let i = 1; i <= n; i++)
-    {
+   for (let i = 1; i <= n; i++) {
+      if (i % 3 == 0 && i % 5 == 0) {
+         result.push("FizzBuzz");
+      } else if (i % 3 === 0) {
+         result.push("Fizz");
+      } else if (i % 5 == 0) {
+         result.push("Buzz");
+      } else {
+         result.push(i.toString());
+      }
+   }
 
-        if (i % 3 ==0 && i % 5 ==0) {
-            result.push("FizzBuzz")
-        }
-
-        else if(i % 3 === 0){
-            result.push("Fizz")
-        }
-        else if (i % 5 == 0) {
-            result.push("Buzz")
-        }
-
-        else {
-            result.push(i.toString())
-        }
-    }
-
-    return result;
-
-    
-
+   return result;
 }
 let n = 20;
-let result = fizzBuzz(n)
+let result = fizzBuzz(n);
 
-console.log(result.join(' '))
+console.log(result.join(" "));
