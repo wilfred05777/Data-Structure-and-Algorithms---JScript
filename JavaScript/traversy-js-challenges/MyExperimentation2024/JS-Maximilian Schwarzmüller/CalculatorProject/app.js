0d0199c48d6bc479782ef147d9c7e8952a424b4c
Wilfred Erdo <wilfred05777@gmail.com>
2024-09-03 14:05:54 +0800

    31. The (Un)Importance of Code Order

---
+++ JavaScript/traversy-js-challenges/MyExperimentation2024/JS-Maximilian Schwarzmüller/CalculatorProject/app.js
@@ -0,0 +1,15 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+function add(num1, num2) {
+   const result = num1 + num2;
+   return result;
+}
+
+currentResult = add(1, 2);
+
+let calculationDescription = `(${defaultResult}+10)*3/2-1 `;
+
+const output = outputResult(currentResult, calculationDescription);
+
+// console.log(output)
