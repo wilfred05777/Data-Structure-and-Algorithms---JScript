bd90cf3a768097f8bc6df34010dd029a0d4c21ab
Wilfred Erdo <wilfred05777@gmail.com>
2024-09-03 13:51:10 +0800

    String and template literals Jonas

---
+++ JavaScript/traversy-js-challenges/MyExperimentation2024/StringAndTemplateLiterals/StringsAndTemplateLiterals.js
@@ -0,0 +1,35 @@
+const wifesFirstname = "Rowena"
+const wifesLastname = "Bancairen"
+const wifesMaidenname = "Batoon"
+
+const wifesAge = 31
+const wifeStatus = "Married"
+const wifesHusband = "Wilfred"
+
+const husbandsFirstname = "Wilfred Erdo"
+const husbandsLastname = "Bancairen"
+const husbandsAge = 38
+
+
+
+const Rowena = `Hi! My name is ${wifesFirstname}  married to ${husbandsFirstname} `
+
+const husbandsName = `Hi! My name is ${husbandsFirstname} married to ${wifesFirstname} ${wifesLastname}`
+
+console.log(Rowena)
+console.log(husbandsName)
+
+console.log('string with \n\ multiple \n\ lines')
+
+console.log(`String
+multiple 
+lines
+using
+back
+ticks`)
+
+
+
+
+
+
