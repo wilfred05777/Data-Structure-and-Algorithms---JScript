571e68e728ab89dce6d9ba3ee5c01a5218406164
Wilfred Erdo Bancairen <wilfred05777@gmail.com>
2024-09-03 16:34:03 +0800

    35. Executing Functions "Indirectly"

--- JavaScript/MyExperimentation2024/JS-Maximilian Schwarzmüller/GlobalAndLocalScope/app.js
+++ JavaScript/MyExperimentation2024/JS-Maximilian Schwarzmüller/GlobalAndLocalScope/app.js
@@ -2,18 +2,24 @@ const defaultResult = 0;
 let currentResult = defaultResult;
 
 function add(num1, num2) {
-   const result = num1 + num2; // local scope 
-   return result; 
+   currentResult = currentResult + userInput.value; // .value will get the value of what the user enter in the Browser input
+
+   outputResult(currentResult, '');
+
+   // const result = num1 + num2; // local scope 
+   // return result; 
    // return result;
 }
 
-currentResult = add(1, 2);
-// alert(result) // local scope call will not work
+addBtn.addEventListener('click', add); // addEventListener => function is buildin the browser
 
-let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
+// currentResult = add(1, 2);
+// // alert(result) // local scope call will not work
+
+// let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
 
 // experimental
 // const outputed = outputResult(currentResult, calculationDescription);
-outputResult(currentResult, calculationDescription);
+// outputResult(currentResult, calculationDescription);
 
-console.log(outputed)
+// console.log(outputed)
