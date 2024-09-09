a17e8027315e6154f6156597c828cb709f86e846
Wilfred Erdo Bancairen <wilfred05777@gmail.com>
2024-09-09 17:42:01 +0800

    arithmetic - learning only

---
+++ JavaScript/MyExperimentation2024/Erdo/calculatev1/calculate.js
@@ -0,0 +1,33 @@
+function calculate() {
+    
+    let num1 = parseFloat(prompt("Enter the first number: "))
+    let num2 = parseFloat(prompt("Enter the second number:"))
+    let operation = prompt("Enter the operation (add, subtract, multiply, divide:)")
+
+
+    let result;
+
+
+    switch (operation) {
+        case 'add':
+            result = num1 + num2
+            break
+        case 'subtract':
+            result = num1 - num2
+            break
+        case 'multiply':
+            result = num1 * num2;
+            break
+        case 'divide':
+            result = num1 / num2;
+            break; 
+        
+        default: 
+            result = "Invalid operation"
+    }
+
+    alert(`The result of ${operation} is: ${result}`)
+}
+
+calculate(2,2)
+
