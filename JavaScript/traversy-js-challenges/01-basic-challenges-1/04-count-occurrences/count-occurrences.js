2d73d30a2a0b55365c58b126587ec3164dc6641e
Wilfred Erdo Bancairen <wilfred05777@gmail.com>
2024-09-10 05:22:29 +0800

    Occurance learning in javascript

--- JavaScript/traversy-js-challenges/01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ JavaScript/traversy-js-challenges/01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,15 +1,37 @@
-function countOccurrences(str, char) {
-   let count = 0;
+/**
+ * This is a program that
+ * Identify a certain phrases words of how many times a letter has repeatedly  appeared
+ */
 
-   for (let i = 0; i < structuredClone.length; i++) {
-      if (str[i] === char) {
-         count++;
-      }
-   }
+// function countOccurrences(str, char) {
+//    let count = 0;
 
-   return count;
-}
+//    for (let i = 0; i < str.length; i++) {
+//       // console.log([str[i]])
+//          if (str[i] === char) {
+//          count++;
+//       }
+//    }
 
-const countOccurrences = (str, char) => str.split(char).length - 1;
+//    // for (let i = 0; i < structuredClone.length; i++) {
+//    //    if (str[i] === char) {
+//    //       count++;
+//    //    }
+//    // }
+
+//    return count;
+// }
+
+
+// another method and shorthand style
+
+const countOccurrences = (str, char) => {
+   // console.log(str.split(''))
+   // console.log(str.split(char)) /// gi split kung unsay unsay naa na lower letter l sa char
+   // console.log(str.split(char).length) /// pag abot sa .length apil ang zero 
+   // console.log(str.split(char).length - 1) /// minus -1 kay remove ang index 0 para dili ma apil sa pag count
+   
+   return str.split(char).length - 1;
+};
 
 module.exports = countOccurrences;
