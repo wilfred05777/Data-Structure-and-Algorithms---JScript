f65871eea1f8ffbc40545aa3be272e71ce6b4ba0
Wilfred Erdo Bancairen <wilfred05777@gmail.com>
2024-09-10 08:47:40 +0800

    findMaxNumber and rename files for learning purposes only

--- JavaScript/traversy-js-challenges/01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ JavaScript/traversy-js-challenges/01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,17 @@
-function findMaxNumber() {}
+// function findMaxNumber(arr) {
+//     return Math.max(...arr);
+// }
+
+function findMaxNumber(arr) {
+    let max = arr[0];
+
+    for (let i = 1; i < arr.length; i++){
+        if (arr[i] > max) {
+            max = arr[i]
+        }
+    }
+
+    return max;
+}
 
 module.exports = findMaxNumber;
