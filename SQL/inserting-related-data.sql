03c12f58d80f0574ec3ddf4ca5017bc27c02e9b8
Wilfred Erdo Bancairen <wilfred05777@gmail.com>
2024-09-24 03:41:37 +0800

    101. Inserting Related Data

--- SQL/inserting-related-data.sql
+++ SQL/inserting-related-data.sql
@@ -1,7 +1,9 @@
--- https://www.udemy.com/course/sql-the-complete-developers-guide-mysql-postgresql/learn/lecture/28877110?start=112#overview
+create database relations;
 
--- https://goalkicker.com/MicrosoftSQLServerBook/
+use relations;
 
+
+--CREATING TABLES START
 CREATE TABLE users(
 	id int primary key identity,
 	first_name varchar(300) not null,
@@ -23,8 +25,27 @@ CREATE TABLE cities(
 	id int primary key identity,
 	name varchar(300) not null
 );
+--CREATING TABLES END --
 
-
-
+-- INSERTING VALUES TO TABLES START --
 INSERT INTO cities(name)
-VALUES('Davao'),('New York'),('Tagum');
+VALUES('Davao'),('New York'),('Tagum'),('Panabo');
+
+
+INSERT INTO addresses(street, house_number, city_id)
+VALUES
+	('San Pedro','8A', 1),
+	('New Jersey','10A', 2),
+	('Apokon','501', 3),
+	('New Visayas','808',4)
+;
+
+INSERT INTO users(first_name,last_name, email, address_id)
+VALUES
+('Wilfred Erdo','Bancairen','wilfed@gmail.com',4),
+('Rowena','Bancairen','Rowena@gmail.com',2),
+('Julian Manuel','Bancairen','JulianManuel@gmail.com',3),
+('Amari Zera','Bancairen','AmariZera@gmail.com',1)
+;
+
+-- INSERTING VALUES TO TABLES END --
