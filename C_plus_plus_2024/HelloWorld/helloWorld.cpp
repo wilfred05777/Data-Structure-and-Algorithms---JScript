f860d026d77b8eabbd2614ec71d44ec39923c45c
Wilfred Erdo Bancairen <wilfred05777@gmail.com>
2024-09-24 09:55:13 +0800

    cpp User-Input

--- C_plus_plus_2024/HelloWorld/helloWorld.cpp
+++ C_plus_plus_2024/HelloWorld/helloWorld.cpp
@@ -1,7 +1,11 @@
 #include <stdio.h>
+#include<iostream>
+
+using namespace std;
 
 int main(){
-    printf("Hello World!");
+    // printf("Hello World!"); // #include <stdio.h>
+    cout << "Hello World!"; // #include<iostream>
 
     return 0;
 }
