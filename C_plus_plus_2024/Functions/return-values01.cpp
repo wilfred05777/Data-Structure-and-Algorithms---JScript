ae4251ae052f8152ae7c363ddb1e225c675c1e22
Wilfred Erdo Bancairen <wilfred05777@gmail.com>
2024-09-24 10:42:13 +0800

    return values

---
+++ C_plus_plus_2024/Functions/return-values01.cpp
@@ -0,0 +1,14 @@
+// https://www.w3schools.com/cpp/cpp_function_return.asp
+
+#include <iostream>
+using namespace std;
+
+int myFunction(int x, int y){
+    return x + y;
+}
+
+int main(){
+    int z = myFunction(5, 3);
+    cout << z;
+    return 0;
+}
